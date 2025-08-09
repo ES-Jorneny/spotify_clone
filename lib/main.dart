21292749c7f70e9a6b6edad24a99e3813bedf69d
ES-Journey <e.s.journey456@gmail.com>
2025-08-09 12:58:29 +0500

    clone

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:spotify_clone/core/utils/theme/app_theme.dart';
 import 'package:spotify_clone/presentation/auth/pages/login_page.dart';
 import 'package:spotify_clone/presentation/auth/pages/reset_password_page.dart';
